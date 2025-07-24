d20c872b8bed441a4ad073b0ce9caf69359a06eb
YuSuBH <yusufsuhail297@gmail.com>
2025-07-24 12:13:12 +0530

    Comlete Basic funtionalities

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
-let note = []
+let notes = []
+
+function loadNotes() {
+  const savedNotes = localStorage.getItem('quickNotes')
+  return savedNotes ? JSON.parse(savedNotes) : []
+}
+
+function saveNote(event) {
+  event.preventDefault()
+
+  const title = document.getElementById('noteTitle').value.trim();
+  const content = document.getElementById('noteContent').value.trim();
+
+  notes.unshift({
+    id: generateId(),
+    title: title,
+    content: content
+  })
+
+  saveNotes()
+  renderNotes()
+}
+
+function generateId() {
+  return Date.now().toString()
+}
+
+function saveNotes() {
+  localStorage.setItem('quickNotes', JSON.stringify(notes))
+}
+
+function renderNotes() {
+  const notesContainer = document.getElementById('notesContainer');
+
+  if(notes.length == 0) {
+    notesContainer.innerHTML = `
+      <div class="empty-state">
+        <h2>No note yet...</h2>
+        <p>Create your first note to get started!</p>
+        <button class="add-note-btn" onclick="openNoteDialog()">+ Add your first Note</button>
+      </div>
+    `
+    return
+  }
+
+  notesContainer.innerHTML = notes.map(note => `
+    <div class="note-card">
+      <h3 class="note-title">${note.title}</h3>
+      <p class='note-content">${note.content}</p>
+    </div>
+    `).join('')
+}
 
 function openNoteDialog() {
   const dialog = document.getElementById('noteDialog');
@@ -14,6 +65,10 @@ function closeNoteDialog() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  notes = loadNotes()
+  renderNotes()
+
+  document.getElementById('noteForm').addEventListener('submit', saveNote)
 
   document.getElementById('noteDialog').addEventListener('click', function(event) {
     if(event.target == this) {
