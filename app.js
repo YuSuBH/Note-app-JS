33a74d390b5a9ef85e57dece2e27f918cd88f004
YuSuBH <yusufsuhail297@gmail.com>
2025-07-24 11:23:16 +0530

    Add three files

---
+++ app.js
@@ -0,0 +1,23 @@
+let note = []
+
+function openNoteDialog() {
+  const dialog = document.getElementById('noteDialog');
+  const titleInput = document.getElementById('noteTitle');
+  const contentInput = document.getElementById('noteContent');
+
+  dialog.showModal()
+  titleInput.focus()
+}
+
+function closeNoteDialog() {
+  document.getElementById('noteDialog').close()
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.getElementById('noteDialog').addEventListener('click', function(event) {
+    if(event.target == this) {
+      closeNoteDialog()
+    }
+  })
+})
